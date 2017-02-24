27b979212dee50f36bd9390aeaa182d5d7567141
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-02-25 00:03:38 +0100

    Make version in updates.js pass semver check

--- updates.js
+++ updates.js
@@ -1,13 +1,13 @@
 {
   "linux-x64-prod": {
     "update": "https://github.com/nukeop/nuclear/releases/download/eddin/nuclear-eddin-linux-x64.AppImage",
-    "version": "eddin",
+    "version": "0.1.0-eddin",
     "platform": "linux",
     "readme": "Please refer to Github readme."
   },
   "win32-x64-prod": {
     "update": "https://github.com/nukeop/nuclear/releases/download/eddin/nuclear-eddin-win-x64.zip",
-    "version": "eddin",
+    "version": "0.1.0-eddin",
     "platform": "win32",
     "readme": "Please refer to Github readme."
   }
